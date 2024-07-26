f493f61b8fff4b42ac4f7dc49ad26704a38429b7
matias <matias_ortiz_@outlook.es>
2024-07-26 16:09:36 -0300

    add styles in chat, icons

--- app/src/main/java/com/mfo/chatapp/ui/MainActivity.kt
+++ app/src/main/java/com/mfo/chatapp/ui/MainActivity.kt
@@ -1,21 +1,19 @@
-package com.mfo.chatapp.ui.home
+package com.mfo.chatapp.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mfo.chatapp.databinding.ActivityHomeBinding
+import com.mfo.chatapp.databinding.ActivityMainBinding
 import com.mfo.chatapp.ui.chat.ChatActivity
 import com.mfo.chatapp.ui.login.LoginActivity
 import com.mfo.chatapp.ui.signup.SignUpActivity
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class HomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initUI()
